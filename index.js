0fc241c346438c16164ec2f5f6b9489582657abe
ibktommy <atomquartz@gmail.com>
2022-06-06 12:10:40 +0100

    Parsing Form Data

--- index.js
+++ index.js
@@ -9,14 +9,30 @@ const app = express()
 app.get('/', (req, res) => {
   res.send(`
     <div>
-      <form>
-        <input type="email" placeholder="Email"/>
-        <input type="passwod" placeholder="Pasword"/>
-        <input type="password" placeholder="Confirm Password"/>
+      <form method="POST">
+        <input name="email" type="email" placeholder="Email"/>
+        <input name="password" type="password" placeholder="Password"/>
+        <input name="confirmPassword" type="password" placeholder="Confirm Password"/>
         <button>Sign Up</button>
       </form>
     </div>
   `)
+})
+
+// Creating A Post request for the Web Server
+app.post('/', (req, res) => {
+  req.on('data', data => {
+    // console.log(data.toString('utf8'))
+    const parsed = data.toString('utf8').split('&')
+    const formData = {}
+    // For Loop to iterate over "Parsed Array"
+    for (let pair of parsed) {
+      const [key, value] = pair.split('=')
+      formData[key] = value;
+    }
+    console.log(formData)
+  })
+  res.send('Account Created!')
 })
 
 // Let App Listen for incoming Network Request from the browser
