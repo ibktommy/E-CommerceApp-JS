5bfa926599cb91251e52bd4f38d20b119bf83341
ibktommy <atomquartz@gmail.com>
2022-06-07 11:18:34 +0100

    Creating an Instance of User Repository to check if we have a file name

--- databaseRepository/users.js
+++ databaseRepository/users.js
@@ -1,3 +1,13 @@
-class Repository {
-  
+// Creating a single class that will be responsible for data access
+class UsersRepository {
+  constructor(filename) {
+    // Condition to check if we have a filename when we create an intance of our Repository
+    if (!filename) {
+      throw new Error('Creating A Repository ')
+    }
+  }
+
 }
+
+// calling an Instance of User Repository
+new UsersRepository()
