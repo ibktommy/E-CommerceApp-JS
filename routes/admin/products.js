6ecc2d25904a16eb77be3fac5a0ad2f2e544599d
ibktommy <atomquartz@gmail.com>
2022-06-15 12:37:13 +0100

    Creating a Products-Page file where new products are created

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -4,13 +4,15 @@ const router = express.Router();
 
 // Requiring the Products Repository
 const productsRepo = require('../../databaseRepository/products')
+// Requiring Products Template
+const newProductsTemplate = require('../../views/admin/products/newProduct')
 
-router.get('./admin/products', (req, res) => {
+router.get('/admin/products', (req, res) => {
 
 })
 
-router.get('./admin/products.new', (req, res) => {
-
+router.get('/admin/products/new', (req, res) => {
+  res.send(newProductsTemplate({}))
 })
 
 module.exports = router
---
+++ views/admin/products/newProduct.js
@@ -0,0 +1,17 @@
+// Requiring the Layout file
+const layout = require('../layout')
+// Requiring the Helper Function File
+const { getError } = require('../../helpers')
+
+module.exports = ({ errors }) => {
+  return layout({
+    content: `
+      <form method="POST">
+        <input placeholder="Product Title" name="title"/>
+        <input placeholder="Product Price" name="price"/>
+        <input type="file" name="image"/>
+        <button>Create Product</button>
+      </form>
+    `
+  })
+}
