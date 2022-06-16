53996d4c199ba23afe2094d6aa091f9501048082
ibktommy <atomquartz@gmail.com>
2022-06-16 17:12:53 +0100

    Creating Products in the ProductsRepo file

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -12,24 +12,26 @@ const newProductsTemplate = require("../../views/admin/products/newProduct");
 // Requiring the Validation Methods
 const { requireTitle, requirePrice } = require("./validator");
 
-router.get("/admin/products", (req, res) => {});
+router.get("/admin/products", (req, res) => { });
 
 // Route Handler - PASSING A NETWORK REQUEST TO THE SERVER FROM THE BROWSER WHEN ON THE "CREATE-NEW-PRODUCT-PAGE"
 router.get("/admin/products/new", (req, res) => {
-	res.send(newProductsTemplate({}));
+  res.send(newProductsTemplate({}));
 });
 // Route Handler - PASSING NETWORK REQUEST FROM THE SERVER BACK TO THE BROWSER
 router.post(
-	"/admin/products/new",
-	[requireTitle, requirePrice],
-	upload.single("image"),
-	(req, res) => {
-		const errors = validationResult(req);
+  "/admin/products/new",
+  [requireTitle, requirePrice],
+  upload.single("image"),
+  async (req, res) => {
+    const errors = validationResult(req);
 
-		const image = req.file.buffer.toString('base64');
+    const image = req.file.buffer.toString('base64');
+    const { title, price } = req.body
+    await productsRepo.create({ title, price, image })
 
-		res.send("Product Created!");
-	},
+    res.send("Product Created!");
+  },
 );
 
 module.exports = router;
