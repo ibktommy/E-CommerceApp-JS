83d2de4d87bd505144d096402576efffb25fe41c
ibktommy <atomquartz@gmail.com>
2022-06-16 15:35:12 +0100

    Installing Multer-Middleware for handling multipart/form-data which is targeted to upload the imge files

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,33 +1,35 @@
 // Require Express Library
 const express = require("express");
 const router = express.Router();
-const { validationResult } = require('express-validator')
+const { validationResult } = require("express-validator");
+const multer = require("multer");
+const upload = multer({ storage: multer.memoryStorage() });
 
 // Requiring the Products Repository
-const productsRepo = require('../../databaseRepository/products')
+const productsRepo = require("../../databaseRepository/products");
 // Requiring Products Template
-const newProductsTemplate = require('../../views/admin/products/newProduct')
-
+const newProductsTemplate = require("../../views/admin/products/newProduct");
 // Requiring the Validation Methods
-const { requireTitle, requirePrice } = require('./validator')
+const { requireTitle, requirePrice } = require("./validator");
 
-router.get('/admin/products', (req, res) => {
-
-})
+router.get("/admin/products", (req, res) => {});
 
 // Route Handler - PASSING A NETWORK REQUEST TO THE SERVER FROM THE BROWSER WHEN ON THE "CREATE-NEW-PRODUCT-PAGE"
-router.get('/admin/products/new', (req, res) => {
-  res.send(newProductsTemplate({}))
-})
+router.get("/admin/products/new", (req, res) => {
+	res.send(newProductsTemplate({}));
+});
 // Route Handler - PASSING NETWORK REQUEST FROM THE SERVER BACK TO THE BROWSER
-router.post('/admin/products/new', [
-  requireTitle,
-  requirePrice
-], (req, res) => {
-  const errors = validationResult(req)
-  console.log(req.body)
+router.post(
+	"/admin/products/new",
+	[requireTitle, requirePrice],
+	upload.single("image"),
+	(req, res) => {
+		const errors = validationResult(req);
 
-  res.send('Product Created!')
-})
+		console.log(req.file);
 
-module.exports = router
+		res.send("Product Created!");
+	},
+);
+
+module.exports = router;
