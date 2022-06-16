76d67a557a217a4e07e7ec2caca315e2e73e2091
ibktommy <atomquartz@gmail.com>
2022-06-16 18:11:06 +0100

    Styling of the newProduct page

--- views/admin/products/newProduct.js
+++ views/admin/products/newProduct.js
@@ -1,19 +1,35 @@
-// Requiring the Layout file
-const layout = require('../layout')
-// Requiring the Helper Function File
-const { getError } = require('../../helpers')
+const layout = require('../layout');
+const { getError } = require('../../helpers');
 
 module.exports = ({ errors }) => {
   return layout({
     content: `
-      <form method="POST" enctype="multipart/form-data">
-        <input placeholder="Product Title" name="title"/>
-        ${getError(errors, 'title')}
-        <input placeholder="Product Price" name="price"/>
-        ${getError(errors, 'price')}
-        <input type="file" name="image"/>
-        <button>Create Product</button>
-      </form>
+      <div class="columns is-centered">
+        <div class="column is-half">
+          <h1 class="subtitle">Create a Product</h1>
+
+          <form method="POST" enctype="multipart/form-data">
+            <div class="field">
+              <label class="label">Title</label>
+              <input class="input" placeholder="Title" name="title">
+              <p class="help is-danger">${getError(errors, 'title')}</p>
+            </div>
+            
+            <div class="field">
+              <label class="label">Price</label>
+              <input class="input" placeholder="Price" name="price">
+              <p class="help is-danger">${getError(errors, 'price')}</p>
+            </div>
+            
+            <div class="field">
+              <label class="label">Image</label>            
+              <input type="file" name="image" />
+            </div>
+            <br />
+            <button class="button is-primary">Create</button>
+          </form>
+        </div>
+      </div>
     `
-  })
-}
+  });
+};
