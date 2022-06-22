aac45814630879d8a75472cac7dd482327821fe7
ibktommy <atomquartz@gmail.com>
2022-06-22 13:45:50 +0100

    Condition to Check if Cart Data Exists

--- routes/carts.js
+++ routes/carts.js
@@ -1,10 +1,20 @@
 const express = require('express')
+const cartsRepo = require('../databaseRepository/carts')
 
 const router = express.Router()
 
 // ROUTE HANDLER TO ADD ITEM TO CART
-router.post('/cart/products', (req, res) => {
-  console.log(req.body.productId)
+router.post('/cart/products', async (req, res) => {
+  // Condition to Check if Cart Data Exists
+  let cart;
+  if (!req.session.cartId) {
+    cart = await cartsRepo.create({ items: [] })
+    req.session.cartId = cart.id
+  } else {
+    cart = await cartsRepo.getOne(req.session.cartId)
+  }
+
+  console.log(cart)
 
   res.send('Product Added to Cart!')
 })
